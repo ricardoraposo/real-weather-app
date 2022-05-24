9e4f294a15b19f6020201df923f0f19ddf0e0ac8
ricardo <ricardorbxx1@gmail.com>
2022-05-24 11:38:04 -0300

    changed http thing

--- src/js/asyncfunctions.js
+++ src/js/asyncfunctions.js
@@ -3,7 +3,7 @@ import config from "./config"
 const apiKey = config.SECRET_API_KEY
 
 async function getLocationData(locationInput) {
-  const fetchData = await fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${locationInput}&limit=5&appid=${apiKey}`, { mode: 'cors' })
+  const fetchData = await fetch(`https://api.openweathermap.org/geo/1.0/direct?q=${locationInput}&limit=5&appid=${apiKey}`, { mode: 'cors' })
   const locationData = await fetchData.json()
   return locationData
 }
@@ -18,7 +18,7 @@ async function getWeather(locationInput) {
 
 async function getCurrentDate (locationInput) {
   const weatherData = await getWeather(locationInput)
-  const response = await fetch(`http://worldtimeapi.org/api/timezone/${weatherData.timezone}`)
+  const response = await fetch(`https://worldtimeapi.org/api/timezone/${weatherData.timezone}`)
   const timezoneData = await response.json()
   return timezoneData
 }
